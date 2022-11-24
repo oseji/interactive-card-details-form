848ade21828b48872738a97cca30572e6cb3abfb
oseji <82390694+oseji@users.noreply.github.com>
2022-11-24 22:02:07 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      width: {
+        110: "450px",
+        85: "380px",
+      },
+    },
+  },
+  plugins: [],
+};
